306aeaa3889f436faf72ad4decb21b0330828ae3
Chintan Shah <chintan.shah12@outlook.com>
2017-01-19 16:39:50 +0530

    Add project files.

---
+++ src/docker-webhook-admin/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace docker_webhook_admin.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
