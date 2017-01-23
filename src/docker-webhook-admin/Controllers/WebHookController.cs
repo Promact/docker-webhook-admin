432718c6a2eb67c5159d952eec688c6988919657
Chintan Shah <chintan.shah12@outlook.com>
2017-01-23 15:33:24 +0530

    removed logging

--- src/docker-webhook-admin/Controllers/WebHookController.cs
+++ src/docker-webhook-admin/Controllers/WebHookController.cs
@@ -67,11 +67,11 @@ namespace Docker.Webhook.Admin.Controllers
             }
             finally
             {
-                string errors = process.StandardError.ReadToEnd();
-                string output = process.StandardOutput.ReadToEnd();
+                //string errors = process.StandardError.ReadToEnd();
+                //string output = process.StandardOutput.ReadToEnd();
 
-                Console.WriteLine(errors);
-                Console.WriteLine(output);
+                //Console.WriteLine(errors);
+                //Console.WriteLine(output);
                 process.Dispose();
             }
             Console.WriteLine("=========Task Executed==========");
